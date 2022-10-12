20efa51b1273feee82650482a4649db4185d7c9d
kmnemon <kmnemon@163.com>
2022-10-12 18:00:32 +0800

    d

--- twosum/twosum.go
+++ twosum/twosum.go
@@ -1,5 +1,25 @@
 package twosum
 
 func twoSum(nums []int, target int) []int {
+	hash := sliceToMap(nums)
+
+	for k, v := range *hash {
+		if (*hash)[target-k] != nil {
+			return []int{v[0], (*hash)[target-k][0]}
+		} else if (len(v) == 2) && (target == 2*k) {
+			return []int{v[0], v[1]}
+		}
+	}
+
 	return nil
+}
+
+func sliceToMap(nums []int) *map[int][]int {
+	hash := make(map[int][]int)
+
+	for i, v := range nums {
+		hash[v] = append(hash[v], i)
+	}
+
+	return &hash
 }
--- twosum/twosum_test.go
+++ twosum/twosum_test.go
@@ -1,15 +1,79 @@
 package twosum
 
-import "testing"
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
 
-func TestTwoSum(t *testing.T) {
+func TestTwoSum1(t *testing.T) {
 	nums := []int{2, 7, 11, 15}
 	target := 9
 
 	output := twoSum(nums, target)
 
-	if output != nil {
+	if !reflect.DeepEqual(output, []int{0, 1}) {
 		t.Error("wrong answer")
 	}
+}
 
+func TestTwoSum2(t *testing.T) {
+	nums := []int{3, 2, 4}
+	target := 6
+
+	output := twoSum(nums, target)
+
+	if !reflect.DeepEqual(output, []int{1, 2}) {
+		t.Error("wrong answer")
+	}
+}
+
+func TestTwoSum3(t *testing.T) {
+	nums := []int{3, 3}
+	target := 6
+
+	output := twoSum(nums, target)
+
+	if !reflect.DeepEqual(output, []int{0, 1}) {
+		t.Error("wrong answer")
+	}
+}
+
+func TestSliceToMap1(t *testing.T) {
+	nums := []int{6, 7, 8, 9}
+
+	hash := sliceToMap(nums)
+
+	if len(*hash) != 4 {
+		t.Error("slice to map length err")
+	}
+
+	if reflect.DeepEqual((*hash)[6], 0) {
+		t.Error("slice to map mapping err")
+	}
+}
+
+func TestSliceToMap2(t *testing.T) {
+	nums := []int{6, 6, 8, 9}
+
+	hash := sliceToMap(nums)
+
+	if len(*hash) != 3 {
+		t.Error("slice to map length err")
+	}
+
+	if !reflect.DeepEqual((*hash)[6], []int{0,1}) {
+		t.Error("slice to map mapping err")
+	}
+}
+
+func TestSliceToMap3(t *testing.T) {
+	// hash := make(map[string]int)
+	ages := map[string]int{
+		"a": 1,
+		"a": 2,
+	}
+
+	fmt.Println(ages)
+	
 }
