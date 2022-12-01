0c8f3d14c61c92d5a07eea1b1e4dd38877d7701f
kmnemon <kmnemon@163.com>
2022-12-01 18:09:58 +0800

    not so good solution

--- addtwosum/addtwosum.go
+++ addtwosum/addtwosum.go
@@ -1,57 +1,141 @@
 package addtwosum
 
+import (
+	"math"
+)
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	return nil
+	i1 := listToReverseInt(l1)
+	i2 := listToReverseInt(l2)
+
+	var l *ListNode
+	if i1 < (math.MaxUint64/10) || i2 < (math.MaxUint64/10) {
+		i := i1 + i2
+		l = intToReverseList(nil, i)
+	} else {
+		l = addNormal(l1, l2)
+	}
+
+	return l
 }
 
-func listToReverseInt(l *ListNode) int {
-	ri := 0
-	factor := 1
+func addNormal(l1 *ListNode, l2 *ListNode) *ListNode {
+	var head ListNode
+	var temp *ListNode = &head
+	flag := 0
+	for l1 != nil && l2 != nil {
+		val := l1.Val + l2.Val
+		if flag == 1 {
+			val += 1
+			flag = 0
+		}
+
+		if val > 9 {
+			val -= 10
+			flag = 1
+		}
+
+		l := ListNode{
+			Val: val,
+		}
+
+		temp.Next = &l
+		temp = &l
+
+		l1 = l1.Next
+		l2 = l2.Next
+	}
+
+	if l1 != nil {
+		temp.Next = l1
+		if flag == 1 {
+			addOne(l1)
+		}
+
+	} else if l2 != nil {
+		temp.Next = l2
+		if flag == 1 {
+			addOne(l1)
+		}
+	} else if flag == 1 {
+		l := ListNode{Val: 1, Next: nil}
+		temp.Next = &l
+	}
+
+	return head.Next
+}
+
+func addOne(list *ListNode) {
+	flag := 1
+	for list != nil {
+		if flag == 1 {
+			list.Val += 1
+			flag = 0
+			if list.Val > 9 {
+				list.Val -= 10
+				flag = 1
+			}
+			if list.Next == nil {
+				l := ListNode{Val: 1, Next: nil}
+				list.Next = &l
+				break
+			}
+		} else {
+			break
+		}
+		list = list.Next
+	}
+}
+
+// func cheatNumber() *ListNode {
+
+// 	ltmp := ListNode{Val: 1}
+// 	for i := 0; i < 5; i++ {
+// 		l := ListNode{Val: 0}
+// 		l.Next = &ltmp
+// 		ltmp = l
+// 	}
+
+// 	l1 := ListNode{Val: 4, Next: &ltmp}
+// 	l2 := ListNode{Val: 6, Next: &l1}
+// 	l3 := ListNode{Val: 5, Next: &l2}
+// 	return &l3
+// }
+
+func listToReverseInt(l *ListNode) uint64 {
+	var ri uint64 = 0
+	var factor uint64 = 1
 	for l != nil {
-		ri += factor * l.Val
+		ri += factor * uint64(l.Val)
 		factor *= 10
 		l = l.Next
 	}
 	return ri
 }
 
-// func intToReverseList(i int) *ListNode {
-// 	// for i != 0 {
-// 	// 	pick := i % 10
-// 	// 	i /= 10
+func intToReverseList(list *ListNode, i uint64) *ListNode {
+	if list == nil {
+		var ol ListNode
+		list = &ol
+	}
 
-// 	// }
-// 	// return nil
-// }
-
-func intToReverseList(list *ListNode, i int) *ListNode {
 	if i != 0 {
-		pick := i % 10
+		pick := int(i % 10)
 		i /= 10
 		l := ListNode{
 			Val: pick,
 		}
 
-		list.Next = intToReverseList(&l, i)
-		return list
+		list.Next = &l
+
+		intToReverseList(&l, i)
+		return list.Next
 	}
 
-	return nil
+	return &ListNode{Val: 0}
 }
-
-// func reverseInt(i int) int {
-// 	ri := 0
-// 	for i != 0 {
-// 		pick := i % 10
-// 		i /= 10
-
-// 		ri = ri*10 + pick
-// 	}
-
-// 	return ri
-// }
--- addtwosum/addtwosum_test.go
+++ addtwosum/addtwosum_test.go
@@ -1,6 +1,7 @@
 package addtwosum
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -10,6 +11,34 @@ import (
 // 		t.Error("reverse int wrong")
 // 	}
 // }
+
+func TestAddTwoNumbers(t *testing.T) {
+	l1 := intToReverseList(nil, 342)
+	l2 := intToReverseList(nil, 465)
+	ll1 := addTwoNumbers(l1, l2)
+	fmt.Printf("%+v\n", ll1)
+
+	// l3 := intToReverseList(nil, 0)
+	// l4 := intToReverseList(nil, 0)
+	// ll2 := addTwoNumbers(l3, l4)
+	// fmt.Printf("%+v\n", ll2)
+
+	// l5 := intToReverseList(nil, 608)
+	// l6 := intToReverseList(nil, 465)
+	// ll3 := addTwoNumbers(l5, l6)
+	// fmt.Printf("%+v\n", ll3)
+
+	// l7 := intToReverseList(nil, 9999999)
+	// l8 := intToReverseList(nil, 9999)
+	// ll4 := addTwoNumbers(l7, l8)
+	// fmt.Printf("%+v\n", ll4)
+
+	// l9 := intToReverseList(nil, 1000000000000000)
+	// l10 := intToReverseList(nil, 465)
+	// ll5 := addTwoNumbers(l9, l10)
+	// fmt.Printf("%+v\n", ll5)
+
+}
 
 func TestListToInt(t *testing.T) {
 	l3 := ListNode{3, nil}
@@ -22,8 +51,17 @@ func TestListToInt(t *testing.T) {
 }
 
 func TestIntToReverseList(t *testing.T) {
-	i := 17
-	var l *ListNode
-	intToReverseList(l, i)
+	var i uint64 = 17
 
+	l := intToReverseList(nil, i)
+	if l.Val != 7 || l.Next.Val != 1 || l.Next.Next != nil {
+		t.Error("int to reverse list wrong")
+	}
+
+}
+
+func TestAddOne(t *testing.T) {
+	l := intToReverseList(nil, 999)
+	addOne(l)
+	fmt.Println(l)
 }
